3975ccbdf091fcc9a1dd19d44f20c696dcc2f257
EksDeeEksDee <150486575+EksDeeEksDee@users.noreply.github.com>
2025-04-27 17:27:09 +0100

    Update Item.cs

--- Item.cs
+++ Item.cs
@@ -8,17 +8,18 @@ namespace DungeonExplorer
         public string Name { get; set; }
         public string Description { get; set; }
 
+        // Constructor for items.
         public Item(string name, string description)
         {
             Name = name;
             Description = description;
         }
-
+        // Method for using items.
         public virtual void Use(Creature target)
         {
             Console.WriteLine($"{Name} has no special use.");
         }
-
+        // Metho for displaying information about item such as name and description.
         public void ShowItemInfo()
         {
             Console.WriteLine($"Item: {Name}\nDescription: {Description}");
